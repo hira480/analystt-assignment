c32ffa5b0d666553523dc38e7739dc900ccaceb4
hira480 <hiramannan8@gmail.com>
2022-07-22 00:34:39 +0600

    initial setup

---
+++ src/components/DataList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DataList = () => {
+    return (
+        <div>
+            <h1 className='text-primary text-center my-3'>Analystt</h1>
+        </div>
+    );
+};
+
+export default DataList;
---
+++ src/hooks/useData.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const useData = () => {
+    const [info, setInfo] = useState([]);
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default useData;
